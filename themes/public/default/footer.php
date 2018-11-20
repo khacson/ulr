5cf8a621cb2bfcd00cd2a53e236e3236e0896bc1
sonnk <khacson2504@gmail.com>
2018-11-20 09:56:29 +0700

    ccm

---
+++ themes/public/default/footer.php
@@ -0,0 +1,197 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="block">
+				<h1 class="block-title">FIREFUMA</h1>
+				<div class="block-body">
+					<figure class="foot-logo">
+						<img src="images/logo-light.png" class="img-responsive" alt="Logo">
+					</figure>
+					<p class="brand-description">
+						Magz is a HTML5 &amp; CSS3 magazine template based on Bootstrap 3.
+					</p>
+					<a href="category.php" class="btn btn-magz white">Liên hệ <i class="ion-ios-arrow-thin-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="block">
+				<h1 class="block-title">Bất động sản<div class="right"><a href="#"><i class="ion-ios-arrow-thin-right"></i></a></div></h1>
+				<div class="block-body">
+					<ul class="tags">
+						<li><a href="#">Nhà đất bán</a></li>
+						<li><a href="#">Cho thuê</a></li>
+						<li><a href="#">Cần mua</a></li>
+						<li><a href="#">Cần thuê</a></li>
+						<li><a href="#">Dự án</a></li>
+						<li><a href="#">Danh bạ</a></li>
+						<li><a href="#">Tư vấn</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="line"></div>
+			<div class="block">
+				<h1 class="block-title">Newsletter</h1>
+				<div class="block-body">
+					<p>By subscribing you will receive new articles in your email.</p>
+					<form class="newsletter">
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="ion-ios-email-outline"></i>
+							</div>
+							<input type="email" class="form-control email" placeholder="Your mail">
+						</div>
+						<button class="btn btn-primary btn-block white">Subscribe</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="block">
+				<h1 class="block-title">Bất động sản mới</h1>
+				<div class="block-body">
+					<article class="article-mini">
+						<div class="inner">
+							<figure>
+								<a href="category.php">
+									<img src="images/news/img12.jpg" alt="Sample Article">
+								</a>
+							</figure>
+							<div class="padding">
+								<h1><a href="category.php">Donec consequat lorem quis augue pharetra</a></h1>
+							</div>
+						</div>
+					</article>
+					<article class="article-mini">
+						<div class="inner">
+							<figure>
+								<a href="category.php">
+									<img src="images/news/img14.jpg" alt="Sample Article">
+								</a>
+							</figure>
+							<div class="padding">
+								<h1><a href="category.php">eu dapibus risus aliquam etiam ut venenatis</a></h1>
+							</div>
+						</div>
+					</article>
+					<article class="article-mini">
+						<div class="inner">
+							<figure>
+								<a href="category.php">
+									<img src="images/news/img15.jpg" alt="Sample Article">
+								</a>
+							</figure>
+							<div class="padding">
+								<h1><a href="category.php">Nulla facilisis odio quis gravida vestibulum </a></h1>
+							</div>
+						</div>
+					</article>
+					<article class="article-mini">
+						<div class="inner">
+							<figure>
+								<a href="category.php">
+									<img src="images/news/img16.jpg" alt="Sample Article">
+								</a>
+							</figure>
+							<div class="padding">
+								<h1><a href="category.php">Proin venenatis pellentesque arcu vitae </a></h1>
+							</div>
+						</div>
+					</article>
+					<a href="#" class="btn btn-magz white btn-block">See All <i class="ion-ios-arrow-thin-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3 col-xs-12 col-sm-6">
+			<div class="block">
+				<h1 class="block-title">Liên kết với chúng tôi</h1>
+				<div class="block-body">
+					<p>Follow us and stay in touch to get the latest news</p>
+					<ul class="social trp">
+						<li>
+							<a href="#" class="facebook">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="twitter">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-twitter-outline"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="youtube">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-youtube-outline"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="googleplus">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-googleplus"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="instagram">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-instagram-outline"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="tumblr">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-tumblr"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="dribbble">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-dribbble"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="linkedin">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-linkedin"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="skype">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-skype"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="rss">
+								<svg><rect width="0" height="0"/></svg>
+								<i class="ion-social-rss"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="line"></div>
+			<div class="block">
+				<div class="block-body no-margin">
+					<ul class="footer-nav-horizontal">
+						<li><a href="index.php">Trang chủ</a></li>
+						<li><a href="termsofuse.php">Điều khoản</a></li>
+						<li><a href="contact.php">Liên hệ</a></li>
+						<li><a href="about.php">Giới thiệu</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="copyright">
+				COPYRIGHT &copy; DELUGE 2017. ALL RIGHT RESERVED.
+				<div>
+					Design <i class="ion-heart"></i> by <a href="http://firefuma.vn/">Firefuma</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
