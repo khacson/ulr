4a34dafda2fdee2e8aa69972b67a5bed6ba0cff3
hoa <lethikieuhoa103@gmail.com>
2018-12-03 21:17:54 +0700

    add

---
+++ application/public/modules/lease/lease.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author 
+ * @copyright 2018
+ */
+class Lease extends CI_Controller {
+    
+	function __construct(){
+		parent::__construct();			
+	    $this->load->model('base_model');
+		$this->site->setTemplate('lease');
+	}
+    function  _remap($method, $params = array()){
+        if(method_exists($this, $method)){
+            return call_user_func_array(array($this, $method), $params);
+        }
+        $this->_view();
+    }
+	function _view(){
+		$data = new stdClass();
+		
+		$content = $this->load->view('view',$data,true);
+        $this->site->write('content',$content,true);
+		$this->site->write('title','',true);
+		$this->site->write('description','',true);
+		$this->site->write('keywords','',true);
+		
+        $this->site->render();
+	}
+	
+}
