a04b83e0f33eb3fd55ed456349d1627374b90538
cdsvn <conduongsang.info@gmail.com>
2019-03-08 14:07:57 +0700

    add new column to

--- files/sql_update.sql
+++ files/sql_update.sql
@@ -1,3 +1,8 @@
 ALTER TABLE `vland_member`
 	ADD COLUMN `rp_token` VARCHAR(256) NULL DEFAULT NULL AFTER `last_login`,
 	ADD COLUMN `rp_token_expired` BIGINT NULL DEFAULT NULL AFTER `rp_token`;
+
+-- Thêm cột chứa token register và thời gian hết hạn của token này
+ALTER TABLE `vland_member`
+	ADD COLUMN `token` VARCHAR(256) NULL DEFAULT NULL AFTER `password`,
+	ADD COLUMN `token_expired` BIGINT NULL DEFAULT NULL AFTER `token`;
