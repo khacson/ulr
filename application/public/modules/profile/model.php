f288d63f44747dd2e44216617a47a48850a1500b
Son Nguyen <khacson2504@gmail.com>
2019-03-08 22:28:02 +0700

    Profile

---
+++ application/public/modules/profile/model.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Sonnk
+ * @copyright 2019
+ */
+class ProfileModel extends CI_Model
+{
+	function __construct(){
+		parent::__construct('');
+	}
+}
---
+++ application/public/modules/profile/views/view.php
@@ -0,0 +1,80 @@
+<section class="search">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<aside>
+							<h2 class="aside-title">Search</h2>
+							<div class="aside-body">
+								<p>Search with other keywords or use filters for more accurate results.</p>
+								<form>
+									<div class="form-group">
+										<div class="input-group">
+											<input type="text" name="q" class="form-control" placeholder="Type something ..." value="hello">
+											<div class="input-group-btn">
+												<button class="btn btn-primary">
+													<i class="ion-search"></i>
+												</button>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</aside>
+						<aside>
+							<h2 class="aside-title">Filter</h2>
+							<div class="aside-body">
+								<form class="checkbox-group">
+									<div class="group-title">Date</div>
+									<div class="form-group">
+										<label><input type="radio" name="date" checked> Anytime</label>
+									</div>
+									<div class="form-group">
+										<label><input type="radio" name="date"> Today</label>
+									</div>
+									<div class="form-group">
+										<label><input type="radio" name="date"> Last Week</label>
+									</div>
+									<div class="form-group">
+										<label><input type="radio" name="date"> Last Month</label>
+									</div>
+									<br>
+									<div class="group-title">Categories</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category" checked> All Categories</label>
+									</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category"> Lifestyle</label>
+									</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category"> Travel</label>
+									</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category"> Computer</label>
+									</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category"> Film</label>
+									</div>
+									<div class="form-group">
+										<label><input type="checkbox" name="category"> Sport</label>
+									</div>
+								</form>
+							</div>
+						</aside>
+					</div>
+					<div class="col-md-9">
+						<div class="nav-tabs-group">
+							<ul class="nav-tabs-list">
+								<li class="active"><a href="#">Tin đã đăng</a></li>
+								<li><a href="#">Tin được duyệt</a></li>
+								<li><a href="#">Tin chưa duyệt</a></li>
+								<li><a href="#">Tin hết hạn</a></li>
+							</ul>
+						</div>
+						
+						<div class="row">
+							--------------
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
