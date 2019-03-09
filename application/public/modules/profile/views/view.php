0598b3663498ac5b4197a2eeb656d6982bcdf6da
khacson2504 <khacson2504@gmail.com>
2019-03-09 14:15:32 +0700

    Update Profile

--- application/public/modules/profile/views/view.php
+++ application/public/modules/profile/views/view.php
@@ -141,56 +141,69 @@
                                 <div class="tab-content">
                                     <div class="tab-pane active mtop10">
                                         <div class="row p-b-15  ">
-                                            <div class="col-sm-6">
+                                            <div class="col-sm-8 col-md-8">
                                                 <div class="form-group">
-                                                    <label>Họ và tên <span class="red">(*)</span></label>
-                                                    <input name="fullname" type="text" class="form-control" value="Nguyễn Tất Huy">
-                                                </div>
-
+                                                    <label class="col-md-4 mtop10">Họ và tên <span class="red">(*)</span></label>
+													<div class="col-md-8 mtop10">
+														<input name="fullname" type="text" class="form-control" value="Nguyễn Tất Huy">
+													</div>
+												</div>	
                                                 <div class="form-group">
-                                                    <label>Tên thường gọi</label>
-                                                    <input name="asfullname" type="text" class="form-control" value = "Huy Nguyễn">
+                                                    <label class="col-md-4 mtop10">Tên thường gọi</label>
+													<div class="col-md-8 mtop10">
+														<input name="asfullname" type="text" class="form-control" value = "Huy Nguyễn">
+													</div>
                                                 </div> 
                                                 <div class="form-group">
-                                                    <label>Ngày sinh</label>
-                                                    <div class="input-group date" data-provide="datepicker" data-date-format="dd-mm-yyyy">
-														<input type="text" name="birthday" value="" id="birthday" class="form-input form-control" />				
-														<div class="input-group-addon">
-															<i class="fa fa-calendar "></i>
+                                                    <label class="col-md-4 mtop10">Ngày sinh</label>
+                                                    <div class="col-md-8 mtop10">
+														<div class="input-group date" data-provide="datepicker" data-date-format="dd-mm-yyyy">
+															<input type="text" name="birthday" value="" id="birthday" class="form-input form-control" />				
+															<div class="input-group-addon">
+																<i class="fa fa-calendar "></i>
+															</div>
 														</div>
 													</div>
 												</div>
 												<div class="form-group">
-                                                    <label>Giới tính&nbsp;&nbsp;&nbsp;&nbsp;</label>
-													<input id="sex_1" type="radio" name="sex" value="1" checked="checked"><label for="sex_1">&nbsp; Nam</label>
-													&nbsp;
-													<input id="sex_2" type="radio" name="sex" value="2"><label for="sex_2">&nbsp; Nữ</label>
-													&nbsp;
-													<input id="sex_3" type="radio" name="sex" value="3"><label for="sex_3">&nbsp; Giới tính khác</label>
-													
+                                                    <label class="col-md-4 mtop10">Giới tính&nbsp;&nbsp;&nbsp;&nbsp;</label>
+													<div class="col-md-8 mtop10">
+														<input id="sex_1" type="radio" name="sex" value="1" checked="checked"><label for="sex_1">&nbsp; Nam</label>
+														&nbsp;
+														<input id="sex_2" type="radio" name="sex" value="2"><label for="sex_2">&nbsp; Nữ</label>
+														&nbsp;
+														<input id="sex_3" type="radio" name="sex" value="3"><label for="sex_3">&nbsp; Giới tính khác</label>
+													</div>
 												</div>
 												<div class="form-group">
-                                                    <label>Tỉnh/Thành phố <span class="red">(*)</span></label>
-                                                    <select class="select2me form-control" id="provinceid" name="provinceid" data-placeholder="Chọn tỉnh thành phố" >
-														<option value=""></option>
-														<option value="1">A</option>
-													</select>
+                                                    <label class="col-md-4 mtop10">Tỉnh/Thành phố <span class="red">(*)</span></label>
+													<div class="col-md-8 mtop10">
+														<select class="select2me form-control" id="provinceid" name="provinceid" data-placeholder="Chọn tỉnh/thành phố" >
+															<option value=""></option>
+															<option value="1">A</option>
+														</select>
+													</div>
 												</div> 
 												<div class="form-group">
-                                                    <label>Quận huyện <span class="red">(*)</span></label>
-													<select class="select2me form-control" id="districid" name="districid" 
-													data-placeholder="Chọn quận huyện" >
-														<option value=""></option>
-														<option value="1">A</option>
-													</select>
+                                                    <label class="col-md-4 mtop10">Quận huyện <span class="red">(*)</span></label>
+													<div class="col-md-8 mtop10">
+														<select class="select2me form-control" id="districid" name="districid" data-placeholder="Chọn quận huyện" >
+															<option value=""></option>
+															<option value="1">A</option>
+														</select>
+													</div>
+												</div>  
+												<div class="form-group">
+                                                    <label class="col-md-4 mtop10">Địa chỉ <span class="red">(*)</span></label>
+													<div class="col-md-8 mtop10">
+														<input name="address" type="text" class="form-control" value = "Nguyễn Huệ">
+													</div>
 												</div> 
 												<div class="form-group">
-                                                    <label>Địa chỉ <span class="red">(*)</span></label>
-                                                    <input name="address" type="text" class="form-control" value = "Nguyễn Huệ">
-												</div> 
-												<div class="form-group">
-                                                    <label>Avatar</label>
-                                                    <input type="button" class="btn btn-next btn-primary btn-avatar" name="save" value="chọn hình">
+                                                    <label class="col-md-4 mtop10">Avatar</label>
+													<div class="col-md-8 mtop10">
+														<input type="button" class="btn btn-next btn-primary btn-avatar" name="save" value="chọn hình">
+													</div>
 												</div> 
                                             </div>
                                         </div>
@@ -202,52 +215,80 @@
 							<!--E Thông tin cá nhân-->
 							<!--S Thông tin liên hệ-->
 							<form action="" method="">                        
-                               
-                                <ul class="nav nav-pills">
+                                <ul class="nav nav-pills mtop10">
                                     <li class="active nav-left" ><a href="#step1" >Thông tin liên hệ</a></li>
                                 </ul>
                                 <div class="tab-content">
                                     <div class="tab-pane active mtop10">
                                         <div class="row p-b-15  ">
-                                            <div class="col-sm-6">
+                                            <div class="col-sm-8 col-md-8">
                                                 <div class="form-group">
-                                                    <label>Điện thoại <span class="red">(*)</span></label>
-                                                    <input name="phone" id="phone" type="text" class="form-control" value="0827 533 555">
+                                                    <label class="col-md-4 mtop10">Điện thoại <span class="red">(*)</span></label>
+													<div class="col-md-8 mtop10">
+														<input name="phone" id="phone" type="text" class="form-control" value="0827 533 555">
+													</div>
                                                 </div>
 
                                                 <div class="form-group">
-                                                    <label>Email <span class="red">(*)</span></label>
-                                                    <input name="email" id="email" type="text" class="form-control" >
+                                                    <label class="col-md-4 mtop10">Email </label>
+                                                    <div class="col-md-8 mtop10">
+														<input name="email" id="email" type="text" class="form-control" >
+													</div>
                                                 </div>
 											   
 												<div class="form-group">
-                                                    <label>Facebook</label>
-                                                    <input name="facebook" id="facebook" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Facebook</label>
+													<div class="col-md-8 mtop10">
+														<input name="facebook" id="facebook" type="text" class="form-control">
+													</div>
 												</div>
 												
 												<div class="form-group">
-                                                    <label>Twitter</label>
-                                                    <input name="twitter" id="twitter" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Zalo</label>
+													<div class="col-md-8 mtop10">
+														<input name="zalo" id="zalo" type="text" class="form-control">
+													</div>
 												</div>
 												
 												<div class="form-group">
-                                                    <label>Youtube</label>
-                                                    <input name="twitter" id="twitter" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Twitter</label>
+                                                    <div class="col-md-8 mtop10">
+														<input name="twitter" id="twitter" type="text" class="form-control">
+													</div>
 												</div>
 												
 												<div class="form-group">
-                                                    <label>Instagram</label>
-                                                    <input name="instagram" id="instagram" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Youtube</label>
+													<div class="col-md-8 mtop10">
+														<input name="twitter" id="twitter" type="text" class="form-control">
+													</div>
 												</div>
 												
 												<div class="form-group">
-                                                    <label>Skype</label>
-                                                    <input name="skype" id="skype" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Instagram</label>
+													<div class="col-md-8 mtop10">
+														<input name="instagram" id="instagram" type="text" class="form-control">
+													</div>
 												</div>
 												
 												<div class="form-group">
-                                                    <label>Zalo</label>
-                                                    <input name="zalo" id="zalo" type="text" class="form-control">
+                                                    <label class="col-md-4 mtop10">Skype</label>
+													<div class="col-md-8 mtop10">	
+														<input name="skype" id="skype" type="text" class="form-control">
+													</div>
+												</div>
+												
+												<div class="form-group">
+													<div class="col-md-4 mtop10"></div>
+													<div class="col-md-8 mtop10">
+														<div class="pull-right">
+															<input type="button" class="btn btn-next btn-primary" name="save" value="Lưu">
+														</div>
+														<div class="pull-left">
+															<input type="button" class=" btn btn-previous btn-default disabled" name="previous" value="Xóa">
+														</div>
+														<div class="clearfix"></div>     
+													</div>   
 												</div>
                                             </div>
                                         </div>
@@ -255,20 +296,6 @@
                                     <!--  End step 1 -->
 
                                 </div>
-
-                                <div class="wizard-footer">
-									<div class="col-md-6">
-										<div class="pull-right">
-											<input type="button" class="btn btn-next btn-primary" name="save" value="Lưu">
-										</div>
-										<div class="pull-left">
-											<div clas="row">
-												<input type="button" class="row btn btn-previous btn-default disabled" name="previous" value="Xóa">
-											</div>
-										</div>
-										<div class="clearfix"></div>     
-									</div>                                   
-                                </div>	
                             </form>
 							<!--E Thông tin liên hệ-->
 							</div>
