fb2d1bbeed8d894cffeec25ad505b29540ce7306
khacson2504 <khacson2504@gmail.com>
2019-03-24 19:46:50 +0700

    Main

--- themes/public/default/js/main.js
+++ themes/public/default/js/main.js
@@ -1,6 +1,18 @@
-$(function(){
-	
-});
+main = function (){	
+	getDataFrom = function (cls){	
+		var objReq = {};
+		$("." + cls).each(function(i) {
+			var id = $(this).attr('id');
+			var val = $(this).val();
+			val = val.replace(/['"]/g, '');
+			if(id != undefined){ 
+				objReq[id] = val;
+			}
+		});
+		return JSON.stringify(objReq);
+	}
+}
+
 function handleSelect2(){
 	if (jQuery().select2) {
 		$('.select2me').select2({
