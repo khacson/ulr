8c67ddfc9d0aef360bc0539b668decfd5ac144fb
khacson2504 <khacson2504@gmail.com>
2019-03-24 10:08:32 +0700

    admin

--- application/admin/includes/breadcrumb/language/en_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lang['trangchu'] = "Home";
-$lang['gioithieu'] = "About";
-$lang['sanpham'] = "Product";
-$lang['tintuc'] = "News";
-$lang['lienhe'] = "Contact";
--- application/admin/includes/breadcrumb/language/vn_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lang['trangchu'] = "Trang chủ";
-$lang['gioithieu'] = "Giới thiệu";
-$lang['sanpham'] = "Sản phẩm";
-$lang['tintuc'] = "Tin tức";
-$lang['lienhe'] = "Liên hệ";
