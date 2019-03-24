f93ff816a8008b95bcc09d8586d132b0f1561b75
khacson2504 <khacson2504@gmail.com>
2019-03-24 20:04:34 +0700

    Check email, phone

--- themes/public/default/js/main.js
+++ themes/public/default/js/main.js
@@ -1,5 +1,5 @@
-main = function (){	
-	getDataFrom = function (cls){	
+main: function (){	
+	getDataFrom: function (cls){	
 		var objReq = {};
 		$("." + cls).each(function(i) {
 			var id = $(this).attr('id');
@@ -10,6 +10,26 @@ main = function (){
 			}
 		});
 		return JSON.stringify(objReq);
+	},
+	checPhone: function (txtPhone){
+		var a = document.getElementById(txtPhone).value;
+		var filter = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+		if (filter.test(a)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	checEmail: function (email){
+		var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+		var valid = emailReg.test(email);
+
+		if(!valid) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
 
