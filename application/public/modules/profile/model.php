b42e7615766fa62461eb0782d03abbbd094deaa1
DaoPD <daopd@fsoft.com.vn>
2019-03-25 22:35:47 +0700

    [LandView] sale-post-1

--- application/public/modules/profile/model.php
+++ application/public/modules/profile/model.php
@@ -8,4 +8,31 @@ class ProfileModel extends CI_Model
 	function __construct(){
 		parent::__construct('');
 	}
+
+	function getListSellCatalog() {
+		$query = $this->model->table('vland_sell_catalog')
+					  ->select('id, catalog_name, friendlyurl')
+					  ->where('isdelete', 0)
+					  ->order_by('ordering', 'ASC')
+					  ->find_all();
+		return (object) $query;
+	}
+
+	function getListProvince() {
+		$query = $this->model->table('vland_province')
+					  ->select('id, province_name, friendlyurl')
+					  ->where('isdelete', 0)
+					  ->order_by('ordering', 'ASC')
+					  ->find_all();
+		return (object) $query;
+	}
+
+	function getListDirection() {
+		$query = $this->model->table('vland_direction')
+					  ->select('id, direction_name, friendlyurl')
+					  ->where('isdelete', 0)
+					  ->order_by('ordering', 'ASC')
+					  ->find_all();
+		return (object) $query;
+	}
 }
